e7109978ceeacd2b260cd7978190fef92b3a957d
Thomas Hirsch <t.hirsch90@gmail.com>
2016-11-23 14:30:09 +0100

    Moved routing code completely into SharkFW-Android

---
+++ library/src/main/java/net/sharksystem/android/protocols/routing/service/LocationService.java
@@ -0,0 +1,89 @@
+package net.sharksystem.android.protocols.routing.service;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.Toast;
+
+public class LocationService extends Service
+{
+    private LocationManager mLocationManager = null;
+
+    private static final String TAG = "LOCATION";
+
+    private static final int LOCATION_INTERVAL = 1 * 60 * 1000; //One minute
+    private static final float LOCATION_DISTANCE = 0;
+
+    LocationListener[] mLocationListeners = new MovingRouterLocationListener[] {
+            new MovingRouterLocationListener(LocationManager.GPS_PROVIDER, this),
+            new MovingRouterLocationListener(LocationManager.NETWORK_PROVIDER, this)
+    };
+
+    @Override
+    public IBinder onBind(Intent arg0)
+    {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate()
+    {
+        initializeLocationManager();
+        try {
+            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE, mLocationListeners[1]);
+        } catch (java.lang.SecurityException ex) {
+            this.requestPermissions();
+        } catch (IllegalArgumentException ex) {
+            Log.d(TAG, "network provider does not exist, " + ex.getMessage());
+        }
+        try {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE, mLocationListeners[0]);
+        } catch (java.lang.SecurityException ex) {
+            this.requestPermissions();
+        } catch (IllegalArgumentException ex) {
+            Log.d(TAG, "gps provider does not exist " + ex.getMessage());
+        }
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        if (mLocationManager != null) {
+            for (int i = 0; i < mLocationListeners.length; i++) {
+                try {
+                    mLocationManager.removeUpdates(mLocationListeners[i]);
+                } catch (Exception ex) {
+                    Log.i(TAG, "fail to remove location listners, ignore", ex);
+                }
+            }
+        }
+    }
+
+    private void initializeLocationManager() {
+        if (mLocationManager == null) {
+            mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+
+    private void requestPermissions() {
+        Toast t = Toast.makeText(this, "please turn on positioning stuff", Toast.LENGTH_LONG);
+        t.setGravity(Gravity.CENTER, 0, 0);
+        t.show();
+        Intent settingsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        settingsIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(settingsIntent);
+    }
+}
---
+++ library/src/main/java/net/sharksystem/android/protocols/routing/service/MovingRouterLocationListener.java
@@ -0,0 +1,128 @@
+package net.sharksystem.android.protocols.routing.service;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKTReader;
+
+import net.sharkfw.knowledgeBase.SharkKBException;
+
+import org.json.JSONException;
+
+import java.util.List;
+
+import net.sharksystem.android.protocols.routing.communication.Utils;
+import de.htw.f4.movingrouter.db.CoordinateContentProvider;
+import de.htw.f4.movingrouter.db.MessageContentProvider;
+import de.htw.f4.movingrouter.db.MessageDTO;
+
+public class MovingRouterLocationListener implements LocationListener {
+
+    Location _lastLocation;
+    CoordinateContentProvider _coordinateContentProvider;
+    MessageContentProvider _messageContentProvider;
+    Context _context;
+
+    public MovingRouterLocationListener(String provider, Context context)
+    {
+        _coordinateContentProvider = new CoordinateContentProvider(context);
+        _messageContentProvider = new MessageContentProvider(context);
+        _lastLocation = new Location(provider);
+        _context = context;
+    }
+
+    @Override
+    public void onLocationChanged(Location location)
+    {
+        _lastLocation.set(location);
+
+        this.updateMovementProfile(location);
+        this.checkMessages(location);
+    }
+
+    @Override
+    public void onProviderDisabled(String provider)
+    {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider)
+    {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
+    }
+
+    private void updateMovementProfile(Location location) {
+        Coordinate coordinate = new Coordinate(location.getLatitude(), location.getLongitude());
+        Geometry geometry = new GeometryFactory().createPoint(coordinate);
+        if (!Utils.isInMovementProfile(geometry, _context)) {
+            _coordinateContentProvider.persist(coordinate);
+        }
+    }
+
+    private void checkMessages(Location location) {
+        try {
+            List<MessageDTO> messages = _messageContentProvider.getAllMessages();
+            for (int i = messages.size() - 1; i >=0; i--) {
+                MessageDTO message = messages.get(i);
+                Geometry geometry = new WKTReader().read(message.getReceiverSpatial().getGeometry().getWKT());
+                if (geometry instanceof Point) {
+                    Location destination = new Location("");
+                    destination.setLatitude(geometry.getCoordinate().x);
+                    destination.setLongitude(geometry.getCoordinate().y);
+                    if (location.distanceTo(destination) < 100) {
+                        // TODO real broadcast
+                        Toast.makeText(_context, "Message broadcast because destination reached", Toast.LENGTH_SHORT).show();
+                        _messageContentProvider.delete(message);
+                    } else {
+                        this.decreaseChecks(message);
+                    }
+                } else if (geometry instanceof LineString) {
+                    // TODO
+                } else if (geometry instanceof Polygon) {
+                    Geometry locationPoint = new GeometryFactory().createPoint(new Coordinate(location.getLatitude(), location.getLongitude()));
+                    if (locationPoint.within(geometry)) {
+                        // TODO real broadcast
+                        Toast.makeText(_context, "Message broadcast because destination reached", Toast.LENGTH_SHORT).show();
+                        _messageContentProvider.delete(message);
+                    }
+                    else {
+                        this.decreaseChecks(message);
+                    }
+                }
+            }
+        } catch (SharkKBException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void decreaseChecks(MessageDTO message) throws JSONException, SharkKBException {
+        long checks = message.getChecks();
+        if (checks > 1) {
+            checks--;
+            message.setChecks(checks);
+            _messageContentProvider.update(message);
+        } else {
+            // TODO real broadcast
+            Toast.makeText(_context, "Message broadcast because time expired", Toast.LENGTH_SHORT).show();
+            _messageContentProvider.delete(message);
+        }
+    }
+}
